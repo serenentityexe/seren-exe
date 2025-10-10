8796ef55e0ac90314bc07388d4d9294a769f1dd8
serenentityexe <serenentityexe@gmail.com>
2025-10-11 00:27:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 // 🔹 Base API endpoint (vercel backend)
 const API_BASE = "https://api-ten-inky-24.vercel.app";
-await fetch(`${API_BASE}/api/game/state`);
-await fetch(`${API_BASE}/api/admin/login`, { ... });
 
 const container = document.getElementById("lines-container");
 const input = document.getElementById("command");
